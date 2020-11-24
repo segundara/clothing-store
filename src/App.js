4835f87a63be1f4883ac26778642b79ed898f1e5
segundara <segundara@gmail.com>
2020-11-25 01:56:01 +0200

    updated app js, removed unused imports

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import FetchHandler from './components/FetchHandler';
-import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
   return (
