f780aaec6d3ce2ec3e283ec87a8e508245e080c3
segundara <segundara@gmail.com>
2021-03-19 09:28:46 +0200

    update to currentListHandler

--- src/components/listingHandler/CurrentListHandler.jsx
+++ src/components/listingHandler/CurrentListHandler.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react'
-import Storage from './DisplayHandler';
 import { useDispatch, useSelector } from "react-redux";
 
 const CurrentListHandler = () => {
@@ -7,41 +6,35 @@ const CurrentListHandler = () => {
     const dispatch = useDispatch();
     const state = useSelector(state => state)
 
-    const showCurrentList = () => {
-        let currentDisplay = []
-        const refinedData = state.data.finalOutput
-        for (let i = 0; i < refinedData.length; i++) {
-            currentDisplay[i] = {}
-            for (let item in refinedData[i]) {
-                currentDisplay[i][item] = refinedData[i][item];
-            }
-        }
-
-        for (let j = 0; j < currentDisplay.length; j++) {
-            const element = currentDisplay[j];
-            element.Product = element.Product.slice(state.data.currentPage * state.data.perPage - state.data.perPage, state.data.currentPage * state.data.perPage)
-        }
-
-        dispatch({
-            type: "GET_CURRENT_LISTING",
-            payload: currentDisplay
-        });
-
-        dispatch({
-            type: "LOADING_DONE"
-        });
-    }
-
     useEffect(() => {
+
+        const showCurrentList = () => {
+            let currentDisplay = []
+            const refinedData = state.data.finalOutput
+            for (let i = 0; i < refinedData.length; i++) {
+                currentDisplay[i] = {}
+                for (let item in refinedData[i]) {
+                    currentDisplay[i][item] = refinedData[i][item];
+                }
+            }
+
+            for (let j = 0; j < currentDisplay.length; j++) {
+                const element = currentDisplay[j];
+                element.Product = element.Product.slice(state.data.currentPage * state.data.perPage - state.data.perPage, state.data.currentPage * state.data.perPage)
+            }
+
+            dispatch({
+                type: "GET_CURRENT_LISTING",
+                payload: currentDisplay
+            });
+        }
+
         showCurrentList()
-    }, [state.data.currentPage, state.data.pageNumbers])
+
+    }, [dispatch, state.data.finalOutput, state.data.perPage, state.data.currentPage])
 
     return (
-        <>
-            {!state.status.isLoading && state.data.currentList.length > 0 && (
-                <Storage />
-            )}
-        </>
+        <></>
     )
 }
 
